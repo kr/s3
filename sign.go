b5180bea29d8ffe02fcc3f72dc6c10fc88ae7a28
Keith Rarick <kr@xph.us>
2012-07-17 23:49:04 -0700

    init

---
+++ sign.go
@@ -0,0 +1,155 @@
+// Package s3 signs HTTP requests according to the method described in
+// http://docs.amazonwebservices.com/AmazonS3/2006-03-01/dev/RESTAuthentication.html.
+package s3
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"io"
+	"net/http"
+	"sort"
+	"strings"
+)
+
+var (
+	amp   = []byte{'&'}
+	dot   = []byte{'.'}
+	nl    = []byte{'\n'}
+	qmark = []byte{'?'}
+	slash = []byte{'/'}
+)
+
+var signParams = map[string]bool{
+	"acl":                          true,
+	"lifecycle":                    true,
+	"location":                     true,
+	"logging":                      true,
+	"notification":                 true,
+	"partNumber":                   true,
+	"policy":                       true,
+	"requestPayment":               true,
+	"torrent":                      true,
+	"uploadId":                     true,
+	"uploads":                      true,
+	"versionId":                    true,
+	"versioning":                   true,
+	"versions":                     true,
+	"website":                      true,
+	"response-content-type":        true,
+	"response-content-language":    true,
+	"response-expires":             true,
+	"response-cache-control":       true,
+	"response-content-disposition": true,
+	"response-content-encoding":    true,
+}
+
+// Keys holds a set of Amazon Security Credentials.
+type Keys struct {
+	AccessKey string
+	SecretKey string
+}
+
+// The default Service used by Sign.
+var DefaultService = &Service{"amazonaws.com"}
+
+// Sign signs an HTTP request with the given S3 keys for use on the service
+// DefaultService.
+//
+// This function is shorthand for DefaultService.Sign(r, k).
+func Sign(r *http.Request, k Keys) {
+	DefaultService.Sign(r, k)
+}
+
+// Service represents an S3-compatible service.
+type Service struct {
+	Domain string // used to derive a bucket name from an http.Request
+}
+
+// Sign signs an HTTP request with the given S3 keys for use on service s.
+func (s *Service) Sign(r *http.Request, k Keys) {
+	h := hmac.New(sha1.New, []byte(k.SecretKey))
+	s.writeSigData(h, r)
+	sig := make([]byte, base64.StdEncoding.EncodedLen(h.Size()))
+	base64.StdEncoding.Encode(sig, h.Sum(nil))
+	r.Header.Set("Authorization", "AWS "+k.AccessKey+":"+string(sig))
+}
+
+func (s *Service) writeSigData(w io.Writer, r *http.Request) {
+	w.Write([]byte(r.Method))
+	w.Write(nl)
+	w.Write([]byte(r.Header.Get("content-md5")))
+	w.Write(nl)
+	w.Write([]byte(r.Header.Get("content-type")))
+	w.Write(nl)
+	if _, ok := r.Header["X-Amz-Date"]; !ok {
+		w.Write([]byte(r.Header.Get("date")))
+	}
+	w.Write(nl)
+	writeAmzHeaders(w, r)
+	s.writeResource(w, r)
+}
+
+func (s *Service) writeResource(w io.Writer, r *http.Request) {
+	s.writeVhostBucket(w, r.Host)
+	path := r.URL.RequestURI()
+	if r.URL.RawQuery != "" {
+		path = path[:len(path)-len(r.URL.RawQuery)-1]
+	}
+	w.Write([]byte(path))
+	s.writeSubResource(w, r)
+}
+
+func (s *Service) writeVhostBucket(w io.Writer, host string) {
+	if i := strings.Index(host, ":"); i != -1 {
+		host = host[:i]
+	}
+	if !strings.HasSuffix(host, "."+s.Domain) {
+		w.Write(slash)
+		w.Write([]byte(strings.ToLower(host)))
+	} else if a := strings.Split(host, "."); len(a) > 3 {
+		w.Write(slash)
+		w.Write([]byte(a[0]))
+		for _, s := range a[1 : len(a)-3] { // omit .s3.amazonaws.com
+			w.Write(dot)
+			w.Write([]byte(s))
+		}
+	}
+}
+
+func (s *Service) writeSubResource(w io.Writer, r *http.Request) {
+	var a []string
+	for k, vs := range r.URL.Query() {
+		if signParams[k] {
+			for _, v := range vs {
+				if v == "" {
+					a = append(a, k)
+				} else {
+					a = append(a, k+"="+v)
+				}
+			}
+		}
+	}
+	sort.Strings(a)
+	p := qmark
+	for _, s := range a {
+		w.Write(p)
+		w.Write([]byte(s))
+		p = amp
+	}
+}
+
+func writeAmzHeaders(w io.Writer, r *http.Request) {
+	var a []string
+	for k, v := range r.Header {
+		k = strings.ToLower(k)
+		if strings.HasPrefix(k, "x-amz-") {
+			a = append(a, k+":"+strings.Join(v, ","))
+		}
+	}
+	sort.Strings(a)
+	for _, h := range a {
+		w.Write([]byte(h))
+		w.Write(nl)
+	}
+}
---
+++ sign_test.go
@@ -0,0 +1,145 @@
+package s3
+
+import (
+	"bytes"
+	"net/http"
+	"testing"
+)
+
+var exKeys = Keys{
+	"AKIAIOSFODNN7EXAMPLE",
+	"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+}
+
+var signTest = []struct {
+	method string
+	url    string
+	more   http.Header
+	expBuf string
+	expSig string
+}{
+	{
+		"GET",
+		"http://johnsmith.s3.amazonaws.com/photos/puppy.jpg",
+		http.Header{
+			"Date": {"Tue, 27 Mar 2007 19:36:42 +0000"},
+		},
+		"GET\n\n\nTue, 27 Mar 2007 19:36:42 +0000\n/johnsmith/photos/puppy.jpg",
+		"AWS AKIAIOSFODNN7EXAMPLE:bWq2s1WEIj+Ydj0vQ697zp+IXMU=",
+	},
+	{
+		"PUT",
+		"http://johnsmith.s3.amazonaws.com/photos/puppy.jpg",
+		http.Header{
+			"Content-Type":   {"image/jpeg"},
+			"Content-Length": {"94328"},
+			"Date":           {"Tue, 27 Mar 2007 21:15:45 +0000"},
+		},
+		"PUT\n\nimage/jpeg\nTue, 27 Mar 2007 21:15:45 +0000\n/johnsmith/photos/puppy.jpg",
+		"AWS AKIAIOSFODNN7EXAMPLE:MyyxeRY7whkBe+bq8fHCL/2kKUg=",
+	},
+	{
+		"GET",
+		"http://johnsmith.s3.amazonaws.com/?prefix=photos&max-keys=50&marker=puppy",
+		http.Header{
+			"User-Agent": {"Mozilla/5.0"},
+			"Date":       {"Tue, 27 Mar 2007 19:42:41 +0000"},
+		},
+		"GET\n\n\nTue, 27 Mar 2007 19:42:41 +0000\n/johnsmith/",
+		"AWS AKIAIOSFODNN7EXAMPLE:htDYFYduRNen8P9ZfE/s9SuKy0U=",
+	},
+	{
+		"GET",
+		"http://johnsmith.s3.amazonaws.com/?acl",
+		http.Header{
+			"Date": {"Tue, 27 Mar 2007 19:44:46 +0000"},
+		},
+		"GET\n\n\nTue, 27 Mar 2007 19:44:46 +0000\n/johnsmith/?acl",
+		"AWS AKIAIOSFODNN7EXAMPLE:c2WLPFtWHVgbEmeEG93a4cG37dM=",
+	},
+	{
+		"DELETE",
+		"http://s3.amazonaws.com/johnsmith/photos/puppy.jpg",
+		http.Header{
+			"User-Agent": {"dotnet"},
+			"Date":       {"Tue, 27 Mar 2007 21:20:27 +0000"},
+			"x-amz-date": {"Tue, 27 Mar 2007 21:20:26 +0000"},
+		},
+		"DELETE\n\n\n\nx-amz-date:Tue, 27 Mar 2007 21:20:26 +0000\n/johnsmith/photos/puppy.jpg",
+
+		// The expected signature in Amazon's documentation,
+		// "AWS AKIAIOSFODNN7EXAMPLE:9b2sXq0KfxsxHtdZkzx/9Ngqyh8=",
+		// appears to be incorrect.
+		"AWS AKIAIOSFODNN7EXAMPLE:R4dJ53KECjStyBO5iTBJZ4XVOaI=",
+	},
+	{
+		"PUT",
+		"http://static.johnsmith.net:8080/db-backup.dat.gz",
+		http.Header{
+			"User-Agent":                   {"curl/7.15.5"},
+			"Date":                         {"Tue, 27 Mar 2007 21:06:08 +0000"},
+			"x-amz-acl":                    {"public-read"},
+			"content-type":                 {"application/x-download"},
+			"Content-MD5":                  {"4gJE4saaMU4BqNR0kLY+lw=="},
+			"X-Amz-Meta-ReviewedBy":        {"joe@johnsmith.net", "jane@johnsmith.net"},
+			"X-Amz-Meta-FileChecksum":      {"0x02661779"},
+			"X-Amz-Meta-ChecksumAlgorithm": {"crc32"},
+			"Content-Disposition":          {"attachment; filename=database.dat"},
+			"Content-Encoding":             {"gzip"},
+			"Content-Length":               {"5913339"},
+		},
+		"PUT\n4gJE4saaMU4BqNR0kLY+lw==\napplication/x-download\nTue, 27 Mar 2007 21:06:08 +0000\nx-amz-acl:public-read\nx-amz-meta-checksumalgorithm:crc32\nx-amz-meta-filechecksum:0x02661779\nx-amz-meta-reviewedby:joe@johnsmith.net,jane@johnsmith.net\n/static.johnsmith.net/db-backup.dat.gz",
+		"AWS AKIAIOSFODNN7EXAMPLE:ilyl83RwaSoYIEdixDQcA4OnAnc=",
+	},
+	{
+		"GET",
+		"http://s3.amazonaws.com/",
+		http.Header{
+			"Date": {"Wed, 28 Mar 2007 01:29:59 +0000"},
+		},
+		"GET\n\n\nWed, 28 Mar 2007 01:29:59 +0000\n/",
+		"AWS AKIAIOSFODNN7EXAMPLE:qGdzdERIC03wnaRNKh6OqZehG9s=",
+	},
+	{
+		"GET",
+		// I've altered this example from the one documented by Amazon
+		// since package http never produces lower-case %-encodings.
+		"http://s3.amazonaws.com/dictionary/fran%C3%A7ais/pr%C3%A9f%C3%A8re",
+		http.Header{
+			"Date": {"Wed, 28 Mar 2007 01:49:49 +0000"},
+		},
+		"GET\n\n\nWed, 28 Mar 2007 01:49:49 +0000\n/dictionary/fran%C3%A7ais/pr%C3%A9f%C3%A8re",
+		"AWS AKIAIOSFODNN7EXAMPLE:81VEw/Bc3GDt/k65Xrrk3AdfI4c=",
+	},
+}
+
+func TestSign(t *testing.T) {
+	for _, ts := range signTest {
+		r, err := http.NewRequest(ts.method, ts.url, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		for k, vs := range ts.more {
+			for _, v := range vs {
+				r.Header.Add(k, v)
+			}
+		}
+		var buf bytes.Buffer
+		DefaultService.writeSigData(&buf, r)
+		if buf.String() != ts.expBuf {
+			t.Errorf("in %s:", r.Method)
+			t.Logf("url %s", r.URL.String())
+			t.Logf("exp %q", ts.expBuf)
+			t.Logf("got %q", buf.String())
+		}
+
+		DefaultService.Sign(r, exKeys)
+		if got := r.Header.Get("Authorization"); got != ts.expSig {
+			t.Errorf("in %s:", r.Method)
+			t.Logf("url %s", r.URL.String())
+			t.Logf("exp %q", ts.expSig)
+			t.Logf("got %q", got)
+		}
+	}
+}
