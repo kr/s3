d8126caaf1b7a9833767398b6af3b1c0ea6b9037
Keith Rarick <kr@xph.us>
2012-07-27 15:53:25 -0700

    add example

---
+++ example_test.go
@@ -0,0 +1,30 @@
+package s3_test
+
+import (
+	"strings"
+	"log"
+	"net/http"
+	"time"
+	"strconv"
+	"fmt"
+	"github.com/kr/s3"
+	"os"
+)
+
+func ExampleSign() {
+	keys := s3.Keys{
+		os.Getenv("S3_ACCESS_KEY"),
+		os.Getenv("S3_SECRET_KEY"),
+	}
+	data := strings.NewReader("hello, world")
+	r, _ := http.NewRequest("PUT", "https://example.s3.amazonaws.com/foo", data)
+	r.Header.Set("Date", time.Now().UTC().Format(http.TimeFormat))
+	r.Header.Set("Content-Length", strconv.Itoa(data.Len()))
+	r.Header.Set("X-Amz-Acl", "public-read")
+	s3.Sign(r, keys)
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp.StatusCode)
+}
